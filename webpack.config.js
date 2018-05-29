9c8ab0dac9ea953dd71f70f3ae7867a7dd2beba5
Anderson de Oliveira <andersonba@me.com>
2018-05-29 14:44:05 -0300

    webpack code improvements

--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,75 @@
-var webpack = require("webpack"),
-    path = require("path"),
-    fileSystem = require("fs"),
-    env = require("./utils/env"),
-    CleanWebpackPlugin = require("clean-webpack-plugin"),
-    CopyWebpackPlugin = require("copy-webpack-plugin"),
-    HtmlWebpackPlugin = require("html-webpack-plugin"),
-    WriteFilePlugin = require("write-file-webpack-plugin");
+const webpack = require('webpack');
+const path = require('path');
+const fileSystem = require('fs');
+const env = require('./utils/env');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const WriteFilePlugin = require('write-file-webpack-plugin');
 
 // load the secrets
-var alias = {};
+const alias = {};
 
-var secretsPath = path.join(__dirname, ("secrets." + env.NODE_ENV + ".js"));
+const secretsPath = path.join(__dirname, (`secrets.${env.NODE_ENV}.js`));
 
-var fileExtensions = ["jpg", "jpeg", "png", "gif", "eot", "otf", "svg", "ttf", "woff", "woff2"];
+const fileExtensions = ['jpg', 'jpeg', 'png', 'gif', 'eot', 'otf', 'svg', 'ttf', 'woff', 'woff2'];
 
 if (fileSystem.existsSync(secretsPath)) {
-  alias["secrets"] = secretsPath;
+  alias.secrets = secretsPath;
 }
 
-var options = {
+const options = {
   target: 'web',
   entry: {
-    index: path.join(__dirname, "src", "index.js"),
+    index: path.join(__dirname, 'src', 'index.js'),
   },
   output: {
-    path: path.join(__dirname, "build"),
-    filename: "[name].bundle.js"
+    path: path.join(__dirname, 'build'),
+    filename: '[name].bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        loader: "style-loader!css-loader",
-        exclude: /node_modules/
+        loader: 'style-loader!css-loader',
+        exclude: /node_modules/,
       },
       {
-        test: new RegExp('\.(' + fileExtensions.join('|') + ')$'),
-        loader: "file-loader?name=[name].[ext]",
-        exclude: /node_modules/
+        test: new RegExp(`.(${fileExtensions.join('|')})$`),
+        loader: 'file-loader?name=[name].[ext]',
+        exclude: /node_modules/,
       },
       {
         test: /\.html$/,
-        loader: "html-loader",
-        exclude: /node_modules/
-      }
-    ]
+        loader: 'html-loader',
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
-    alias: alias
+    alias,
   },
   plugins: [
     // clean the build folder
-    new CleanWebpackPlugin(["build"]),
+    new CleanWebpackPlugin(['build']),
     // expose and write the allowed env vars on the compiled bundle
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV)
+      'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV),
     }),
     new CopyWebpackPlugin([{
-      from: "src/manifest.json",
-      transform: function (content, path) {
-        // generates the manifest file using the package.json informations
-        return Buffer.from(JSON.stringify({
+      from: 'src/manifest.json',
+      transform: content =>
+        Buffer.from(JSON.stringify({
           description: process.env.npm_package_description,
           version: process.env.npm_package_version,
-          ...JSON.parse(content.toString())
-        }))
-      }
+          ...JSON.parse(content.toString()),
+        })),
     }]),
-    new WriteFilePlugin()
-  ]
+    new WriteFilePlugin(),
+  ],
 };
 
-if (env.NODE_ENV === "development") {
-  options.devtool = "cheap-module-eval-source-map";
+if (env.NODE_ENV === 'development') {
+  options.devtool = 'cheap-module-eval-source-map';
 }
 
 module.exports = options;
